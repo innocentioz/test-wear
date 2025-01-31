0cb572b8d0e3ce3313df627e0feea6515be73a23
Stepan Batalov <troublezhahahaha@gmail.com>
2025-01-31 10:19:21 +0300

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ import { SessionProvider } from "next-auth/react";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="yandex-verification" content="2eda3b196392eab2" />
+        <meta name="google-site-verification" content="P_GscIXEXTPBMfYsHkK6ICiVDayViIrBfhqJETYA-w0" />
+      </head>
       <body className="">
         <SessionProvider>
           <CartProvider>
